8c7e62846cf23ae43840c4dd01251463265e613c
Amanda Ogletree <amandaogletree19@gmail.com>
2025-12-18 23:44:27 -0500

    Started logic for Todo Item.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,10 +1,33 @@
+import { useContext, useState } from "react"
+import type { TodoItemProps } from "../types"
+import { TodoContext } from "../contexts/contexts"
 
 
-export default function TodoItem(){
-    
+export default function TodoItem({todo}:TodoItemProps){
+    const {editTodo, deleteTodo} = useContext(TodoContext)
+    const [isEditing, setIsEditing] = useState(false)
+    const [editText, setEditText] = useState(todo.text)
+
+    const handleEdit = (event:React.MouseEvent<HTMLParagraphElement | HTMLButtonElement>) =>{
+        const target = event.currentTarget
+
+        if 
+    }
+
     return (
-        <>
-        
+        <>  {
+                isEditing? 
+                <div className="todo-item-container">
+                    <input type="text">{todo.text}</input>
+                </div> : 
+                <div className="todo-item-container">
+                    <p className="todo-item">{todo.text}</p>
+                    <button>📝</button>
+                    <button>🗑</button>
+                </div>
+                
+                }
+
         </>
     )
 }
