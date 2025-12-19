226d760ee9b97f693fd9654e49e4dfe048562e2c
Amanda Ogletree <amandaogletree19@gmail.com>
2025-12-18 21:24:31 -0500

    Added context, logic, variables, form validation, and context to TodoInput.

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,11 +1,45 @@
+import { useContext, useState } from "react"
+import { TodoContext } from "../contexts/contexts"
+import type { Todo } from "../types"
 
 
 
 export default function TodoForm(){
+    const {addTodo} = useContext(TodoContext)
+    const [text, setText] = useState('')
+
+    const formValidation = text.trim() !== '' //On valid when there is text.
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        const inputText = event.target.value
+        setText(inputText)
+    }
+
+    const handleAddTodo = (event: React.SyntheticEvent<HTMLFormElement>) =>{
+        event?.preventDefault()
+        //check if field is valid
+        if (!formValidation){
+            return alert(`You haven't entered anything! Nice try.🤨`)
+        }
+
+        //Create Todo
+        const newId:string = `${text[0]}${Date.now()}`
+        const newTodo:Todo = {
+            id: newId,
+            text: text,
+            completed: false
+        }
+        const form = event.currentTarget
+        const newTodoForm = new FormData(form)
+        addTodo(newTodo)
+        form.reset()
+        alert('New Todo Added! Look at that prodcutivity! 🎉')
+    }
+
     return(
         <>
-            <form className="simple-todo-form-container">
-                <input className="todo-input" type="text" name="text" value={''}/>
+            <form onSubmit={handleAddTodo} className="simple-todo-form-container">
+                <input onChange={handleInputChange} className="todo-input" type="text" name="text" value={text}/>
                 <button className="todo-submit-button" type='submit'>Add Todo</button>
             </form>
         </>
