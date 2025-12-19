381c27e515a0878782db5ae4ac86a1b05bd6ef0c
Amanda Ogletree <amandaogletree19@gmail.com>
2025-12-19 00:09:00 -0500

    Added context, variables, useStates, and logic for conditional JSX for Todo Item rendering.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,22 +8,43 @@ export default function TodoItem({todo}:TodoItemProps){
     const [isEditing, setIsEditing] = useState(false)
     const [editText, setEditText] = useState(todo.text)
 
+
+    const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        const editText = event.target.value
+        setEditText(editText)
+    }
+
+
     const handleEdit = (event:React.MouseEvent<HTMLParagraphElement | HTMLButtonElement>) =>{
         const target = event.currentTarget
 
-        if 
+        setIsEditing(true) //Set to true on both events so the edit input shows
+        editTodo(todo.id, editText)
+    }
+
+    const handleConfirm = (event:React.KeyboardEvent<HTMLInputElement> &  React.FocusEvent<HTMLInputElement>)=>{
+        if(event.key === 'Enter'){
+            setIsEditing(false)
+        }
+        else if(event.currentTarget.onblur){
+            setIsEditing(false)
+        }
+    }
+
+    const handleDeletion = () =>{
+        deleteTodo(todo.id)
     }
 
     return (
         <>  {
                 isEditing? 
                 <div className="todo-item-container">
-                    <input type="text">{todo.text}</input>
+                    <input className="edit-item" onChange={handleChangeInput} onDoubleClick={handleEdit} onKeyUp={handleConfirm} type="text">{editText}</input>
                 </div> : 
                 <div className="todo-item-container">
                     <p className="todo-item">{todo.text}</p>
-                    <button>📝</button>
-                    <button>🗑</button>
+                    <button onClick={handleEdit} className="edit-button">📝</button>
+                    <button onClick={handleDeletion} className="delete-button">🗑</button>
                 </div>
                 
                 }
